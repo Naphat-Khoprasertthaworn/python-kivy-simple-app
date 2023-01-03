d8ac3add955a53c88f1db707a97de44f6c6410b8
Naphat Khoprasertthaworn <napat58021@gmail.com>
2023-01-03 09:38:33 +0700

    first commit

---
+++ proj1/main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+
+root = Tk()
+
+root.title("gui2")
+root.option_add("*Font","consolas 25")
+Label(root,text="hello world",fg="red").pack()
+
+
+
+# root.geometry("500x400")
+
+root.mainloop()
